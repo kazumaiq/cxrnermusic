e1995993374f9b1391e00a379064fe8a2ded5289
kazumaiq <kazumaiq@users.noreply.github.com>
2026-02-12 21:47:08 +0600

    Make home page dynamic for live featured updates

--- app/page.tsx
+++ app/page.tsx
@@ -11,6 +11,9 @@ import { artists, faqItems, services, stats, testimonials, whyUs } from "../data
 import { getFeaturedRelease } from "../lib/featured";
 import { getReleases } from "../lib/releases";
 
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+
 export default async function HomePage() {
   const featured = await getFeaturedRelease();
   const releases = await getReleases();
